f11a89f40f1b87142f7311ff0df79cded0998a86
CesarPereaJimenez <55038141+CesarPereaJimenez@users.noreply.github.com>
2019-11-06 19:04:17 -0600

    Add files via upload

---
+++ examen web/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Formulas</h1>
+    <a href="rectangulo.php">Rectangulo</a>
+    <a href="circulo.php">Circulo</a>
+    <h2>Nombres:</h2>
+    <h3>Cesar Perea Jimenez<h3>
+    <h3>Ana Karen Paz Campero<h3>
+</body>
+</html>
---
+++ examen web/rectangulo.php
@@ -0,0 +1,17 @@
+
+<html>
+<head><title>Area del Rectángulo</title></head>
+<body bgcolor="lightpink">
+<font size="+1">
+<form action="http://localhost/examen%20web/resultado.php" method="POST">
+Introduzca el valor de la base del rectangulo: <br>
+  <input type="text" size=2 name="base"> <br>
+Introduzca el valor de la altura del rectangulo: <br>
+  <input type="text" size=2 name="altura"> <br> 
+
+    <input type=submit value="Resolver">
+ </form>
+ <br>
+ </body>
+ </html>
+
